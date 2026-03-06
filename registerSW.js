cea86c7011ab7fabfa7bbebf6ca40de9d4e77a20
Muhammad Farooq <enmuhammadfarooq@gmail.com>
2026-03-06 15:53:36 +0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Maha-Zainab-USA-Portfolio/sw.js', { scope: '/Maha-Zainab-USA-Portfolio/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
